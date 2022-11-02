ca9fd92f1ab7df836d6430303daf9c65a6273b37
Park Junha <qkrwnsgk1624@naver.com>
2022-11-02 16:58:10 +0900

    Image Crop & UI Base

---
+++ imgcrop.py
@@ -0,0 +1,22 @@
+from PIL import Image
+import os
+
+def imgcrop(input, save_path, xPieces, yPieces):
+    filename, file_extension = os.path.splitext(input)
+    im = Image.open('image/' + input)
+    imgwidth, imgheight = im.size
+    height = imgheight // yPieces
+    width = imgwidth // xPieces
+    for i in range(0, yPieces):
+        for j in range(0, xPieces):
+            box = (j * width, i * height, (j + 1) * width, (i + 1) * height)
+            a = im.crop(box)
+            fname = filename + "_" + str(i) + "_" + str(j) + file_extension
+            savename = save_path + fname
+            print(savename)
+            try:
+                a.save(savename)
+            except:
+                pass
+
+imgcrop("corgi.jpg", 'cropimg/' , 3, 3)
